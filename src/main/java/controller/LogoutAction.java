83236be5528613650c72ca51b2adbd5ef64d0978
하정희 <hajeonghui@gmail.com>
2024-01-14 00:32:33 +0900

    [update] 컨트롤러 로그아웃 기능 작성

--- src/main/java/controller/LogoutAction.java
+++ src/main/java/controller/LogoutAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LogoutAction implements Action {
 
@@ -14,7 +15,19 @@ public class LogoutAction implements Action {
 
 		ActionForward forward = new ActionForward();
 		
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session=request.getSession();
+		session.removeAttribute("member");
+		
+		forward.setPath("mainPage.do"); 
+		forward.setRedirect(true);
+		//수정예정: 모달창 문구 mainPage.do에서 출력 예정
+		//request.setAttribute("msg", "로그아웃이 완료되었습니다! :D");
+		//forward.setRedirect(false);
 		return forward;
+	
 	}
+	
 
 }
