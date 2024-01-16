fd6bfb0bc4943936c052ad0decfe4341f23bcf65
하정희 <hajeonghui@gmail.com>
2024-01-16 18:03:15 +0900

    [CTRL] 문자인증번호 로직 타입 수정

--- src/main/java/controller/CheckTel.java
+++ src/main/java/controller/CheckTel.java
@@ -39,11 +39,11 @@ public class CheckTel extends HttpServlet {
         * 뷰 -- 전화번호 3개로 --> 컨트롤러
         * 컨트롤러 -- 인증번호(요청 파라미터) --> 뷰 (인증번호 확인은 뷰가)
 		*/
-		Integer phoneNumber1=Integer.parseInt(request.getParameter("phoneNum1"));
-		Integer phoneNumber2=Integer.parseInt(request.getParameter("phoneNum2"));
-		Integer phoneNumber3=Integer.parseInt(request.getParameter("phoneNum3"));
+		String phoneNumber1=(String)request.getParameter("phoneNum1");
+		String phoneNumber2=(String)request.getParameter("phoneNum2");
+		String phoneNumber3=(String)request.getParameter("phoneNum3");
 		
-		int combinedPhoneNumber=phoneNumber1+phoneNumber2+phoneNumber3;
+		String combinedPhoneNumber=phoneNumber1+phoneNumber2+phoneNumber3;
 		
 		
 		Random random  = new Random();
@@ -60,7 +60,8 @@ public class CheckTel extends HttpServlet {
 		// Message 패키지가 중복될 경우 net.nurigo.sdk.message.model.Message로 치환하여 주세요
 		Message message = new Message();
 		message.setFrom("01033157366");
-		message.setTo("combinedPhoneNumber");
+		message.setTo(combinedPhoneNumber);
+
 		message.setText("내또코 쇼핑몰 인증번호 ["+numStr+"]을 입력해주세요.");
 
 		try {
