f233526e17d0f78b61d2b8b2f1edaab52a928a07
강준현 <59286081+nawaraing@users.noreply.github.com>
2024-01-22 13:45:33 +0900

    [ctrl] ProductAllPageAction 오류 수정

--- src/main/java/controller/ProductAllPageAction.java
+++ src/main/java/controller/ProductAllPageAction.java
@@ -21,16 +21,38 @@ public class ProductAllPageAction implements Action{
 		ProductDTO pDTO = new ProductDTO();
 		ProductDAO pDAO = new ProductDAO();
 		
-		pDTO.setSearchCondition("상품목록페이지");
-		pDTO.setAncSelectMin(1);
-		pDTO.setAncSelectMax(9);
+		// 전체 페이지 수 확인
+		pDTO.setSearchCondition("상품출력전체");
 		pDTOs = pDAO.selectAll(pDTO);
+		int totalPages = pDTOs.size() / 9;
+		if (pDTOs.size() % 9 != 0) {
+			totalPages++;
+		}
+		request.setAttribute("totalPages", totalPages);
+		
+		// 상품 목록 가져오기
+		String p = request.getParameter("page");
+		System.out.println("String p : " + p);
+		int curPage = 1;
+		if (isNumber(p)) {
+			curPage = Integer.parseInt(p);
+		}
+		
+		pDTO.setSearchCondition("상품목록페이지");
+		
+		pDTO.setAncSelectMin((curPage - 1) * 9 + 1);
+		pDTO.setAncSelectMax((curPage - 1) * 9 + 9);
+		pDTOs = pDAO.selectAll(pDTO);
+		System.out.println("pDTOs : " + pDTOs);
 		request.setAttribute("pDTOs", pDTOs);
 
-		forward.setPath("productDetail.jsp");
+		forward.setPath("productAll.jsp");
 		forward.setRedirect(false);
 		
 		return forward;
 	}
 
+	public static boolean isNumber(String strValue) {
+		return strValue != null && strValue.matches("[-+]?\\d*\\.?\\d+");
+	}
 }
