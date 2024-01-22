031328dcdeb924f3a598a0554192ab00839bfdc5
강준현 <59286081+nawaraing@users.noreply.github.com>
2024-01-22 10:30:16 +0900

    [ctrl] productAllPageAction 기능 개발

--- src/main/java/controller/ProductAllPageAction.java
+++ src/main/java/controller/ProductAllPageAction.java
@@ -1,19 +1,33 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.ProductDAO;
+import model.dto.ProductDTO;
 
 public class ProductAllPageAction implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	
 		ActionForward forward = new ActionForward();
 		
+		ArrayList<ProductDTO> pDTOs = new ArrayList<ProductDTO>();
+		ProductDTO pDTO = new ProductDTO();
+		ProductDAO pDAO = new ProductDAO();
+		
+		pDTO.setSearchCondition("상품목록페이지");
+		pDTO.setAncSelectMin(1);
+		pDTO.setAncSelectMax(9);
+		pDTOs = pDAO.selectAll(pDTO);
+
+		forward.setPath("productDetail.jsp");
+		forward.setRedirect(false);
 		
 		return forward;
 	}
