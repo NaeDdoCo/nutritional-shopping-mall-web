3ac0e5ff5fa442f5316611c4039089e1aee144c4
하정희 <hajeonghui@gmail.com>
2024-01-25 13:54:43 +0900

    [ctrl ]CartPageAction 장바구니 페이지 작성

--- src/main/java/controller/action/CartPageAction.java
+++ src/main/java/controller/action/CartPageAction.java
@@ -1,13 +1,18 @@
 package controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.common.Action;
 import controller.common.ActionForward;
+import model.dao.CartDAO;
+import model.dto.CartDTO;
+
 
 public class CartPageAction implements Action{
 
@@ -16,10 +21,24 @@ public class CartPageAction implements Action{
 			throws ServletException, IOException {
 
 		ActionForward forward = new ActionForward();
+		CartDTO cDTO = new CartDTO();
+		CartDAO cDAO = new CartDAO();
+		HttpSession session = request.getSession();
+
+		//세션에 로그인한 mid로 장바구니 DB목록 가져오기
+		String mid = (String)session.getAttribute("member");
 		
-		
-		
-		return null;
+		cDTO.setMid(mid);
+		cDTO.setSearchCondition("장바구니목록출력");
+		ArrayList<CartDTO> cartList = cDAO.selectAll(cDTO);
+		System.out.println("[log] cartList : "+ cartList);
+
+		request.setAttribute("cartList", cartList);
+
+		forward.setPath("cart.jsp");
+		forward.setRedirect(false);
+
+		return forward;
 	}
 
 }
