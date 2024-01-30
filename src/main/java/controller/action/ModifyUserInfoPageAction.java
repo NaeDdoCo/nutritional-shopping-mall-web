0c444d9361cf5a235892ce77fa72cad1af84396a
강준현 <59286081+nawaraing@users.noreply.github.com>
2024-01-30 10:48:18 +0900

    [CTRL] action 추가

---
+++ src/main/java/controller/action/ModifyUserInfoPageAction.java
@@ -0,0 +1,26 @@
+package controller.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.common.Action;
+import controller.common.ActionForward;
+import model.dao.BuyInfoDAO;
+import model.dto.BuyInfoDTO;
+
+public class ModifyUserInfoPageAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ActionForward forward = new ActionForward();
+		
+		return forward;
+	}
+
+}
