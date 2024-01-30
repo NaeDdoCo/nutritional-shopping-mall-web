7ad79fbaadd3688efb48b1ec3be40354e5a22235
강준현 <59286081+nawaraing@users.noreply.github.com>
2024-01-30 09:47:52 +0900

    [CTRL] BuyInfoPageAction 작성

--- src/main/java/controller/action/BuyInfoPageAction.java
+++ src/main/java/controller/action/BuyInfoPageAction.java
@@ -1,21 +1,36 @@
 package controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.common.Action;
 import controller.common.ActionForward;
+import model.dao.BuyInfoDAO;
+import model.dto.BuyInfoDTO;
 
 public class BuyInfoPageAction implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		ActionForward forward = new ActionForward();
+		
+		BuyInfoDTO bDTO = new BuyInfoDTO();
+		ArrayList<BuyInfoDTO> bDTOs = new ArrayList<BuyInfoDTO>();
+		BuyInfoDAO bDAO = new BuyInfoDAO();
+		
+		HttpSession session = request.getSession();
+		String MID = (String)session.getAttribute("member");
+		
+		bDTO.setMID(MID);
+		bDTOs = bDAO.selectAll(bDTO);
+		System.out.println("[BuyInfoPageAction] bDTOs : " + bDTOs);
+		request.setAttribute("bDTOs", bDTOs);
 		
 		return forward;
 	}
