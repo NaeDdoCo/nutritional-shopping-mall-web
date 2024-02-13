eb640203e085ae485a7ee3449346a2b69fbcdff8
정훈석 <wjdgnstjr93@gmail.com>
2024-02-13 10:04:24 +0900

    [model] 제품상세페이지 리뷰에 이미지 이름 추가선택

--- src/main/java/model/dao/ReviewDAO.java
+++ src/main/java/model/dao/ReviewDAO.java
@@ -27,7 +27,7 @@ public class ReviewDAO {
 	// 상품 리뷰
 	// 상품 세부정보페이지의 하단에 해당 상품에 대한 리뷰를 출력
 	// 최근 리뷰 순으로 정렬
-	private static final String SELECTALL_REVIEW_PRODUCT = "SELECT R.R_ID, R.M_ID, R.B_ID, R.SCORE, R.CONTENTS, R.CREATE_TIME, B.P_ID, P.P_NAME, P.IMAGE_PATH "
+	private static final String SELECTALL_REVIEW_PRODUCT = "SELECT R.R_ID, R.M_ID, R.B_ID, R.SCORE, R.CONTENTS, R.CREATE_TIME, B.P_ID, P.P_NAME, R.IMAGENAME "
 			+ "FROM REVIEW R " + "JOIN BUYINFO B ON B.B_ID = R.B_ID " + "JOIN PRODUCT P ON B.P_ID = P.P_ID "
 			+ "WHERE B.P_ID =? " + "ORDER BY R.CREATE_TIME DESC";
 
@@ -168,7 +168,7 @@ public class ReviewDAO {
 					reviewDTO.setAncCreateTime(createTime);
 					reviewDTO.setAncPID(rs.getInt("P_ID"));
 					reviewDTO.setAncPName(rs.getString("P_NAME"));
-					reviewDTO.setAncImagePath(rs.getString("IMAGE_PATH"));
+					reviewDTO.setImageName(rs.getString("IMAGENAME"));
 					// 하나의 리뷰 데이터가 저장된 객체를 리스트에 저장
 					reviewList.add(reviewDTO);
 				}
